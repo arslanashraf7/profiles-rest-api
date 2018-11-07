c0f223f12ea2db342901d7876444bceab458005e
Arslan Ashraf <arslan.ashraf@arbisoft.com>
2018-11-07 16:50:23 +0500

    Created first apiview

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class HelloApiView(APIView):
+     """Test api view"""
+
+     def get(self, request, format=None):
+         """This returns the data that we want to return"""
+
+         an_apiView = [
+            'Uses HTTP methods as functions(ger, port,put,patch,delete).',
+            'It is similar to traditional Django view.',
+            'Gives you the most control over your logic.',
+            'Is mapped manually to Urls.',
+         ]
+
+
+         return Response({'message': 'Hello!', 'an_apiView': an_apiView})
